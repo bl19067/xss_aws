43c2998f879ca0b970c919b42420af37e0cd9e9b
bl19067 <bl19067@cc.it-hiroshima.ac.jp>
2023-02-06 22:59:26 +0900

    add_photo

--- test4_req.php
+++ test4_req.php
@@ -2,6 +2,7 @@
 <!doctype html>
 
     <p3>XSSの対策をしたが、まだ脆弱性があるみたい...</p3></br>
+    <img src="./images/script.php" alt="code"></br>
     <p3>'0'か'1'が表示される任意スクリプトを実行すると...</p3>
     <form method="POST" action="./test4_res.php">
        <input type="text" name="data"/>
